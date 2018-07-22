71f61e58d1699e6c33c0a4c36070608c08c60454
Lindsey Stanek <lstanek4@gmail.com>
2018-07-22 16:38:52 -0400

    feat: spacing adjustments between sections

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,13 +3,16 @@ const dropdownTitle = document.querySelectorAll('.research__item__dropdown-title
 // Adds a click event to each title to allow the body to slide down
 dropdownTitle.forEach((title) => {
   title.addEventListener('click', () => {
-    let dropdownBody = title.nextElementSibling;
+    const dropdownBody = title.nextElementSibling;
+
     if (dropdownBody.classList.contains('expanded')) {
       dropdownBody.classList.remove('expanded');
       dropdownBody.setAttribute('aria-hidden', true);
+      title.firstElementChild.classList.remove('rotate');
     } else {
       dropdownBody.classList.add('expanded');
       dropdownBody.setAttribute('aria-hidden', false);
+      title.firstElementChild.classList.add('rotate');
     }
   });
 });
